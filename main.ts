53ec5423a32bf3a1771c978197387bca164cc02e
a-flower03 <111801629+a-flower03@users.noreply.github.com>
2022-08-29 14:34:37 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("E D G F B A C5 B ", 120)
+})
+basic.forever(function () {
+	
+})
